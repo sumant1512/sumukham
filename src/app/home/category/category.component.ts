3e2a1c9365cfbebf846fab5b006044e837bd9f9d
sumant1512 <sumantmishra511@gmail.com>
2025-08-22 00:03:12 +0530

    basic pages added

--- src/app/home/category/category.component.ts
+++ src/app/home/category/category.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent {
-
+  categories = [
+    {
+      title: 'Healing Crystals',
+      image: 'crystal.png',
+    },
+    {
+      title: 'Rudraksha',
+      image: 'rudraksh.png',
+    },
+    {
+      title: 'Brass Essentials',
+      image: 'brass.png',
+    },
+    {
+      title: 'Pujan Samagri',
+      image: 'pujan_samagri.png',
+    },
+    {
+      title: 'Fragrances',
+      image: 'fragrancies.png',
+    },
+  ];
 }
