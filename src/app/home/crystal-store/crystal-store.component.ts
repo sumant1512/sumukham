90e4a876778ddf0334164c35cda029ffb191b4b8
sumant1512 <sumantmishra511@gmail.com>
2025-09-06 23:28:25 +0530

    added crystal and rudraksh store

--- src/app/home/crystal-store/crystal-store.component.ts
+++ src/app/home/crystal-store/crystal-store.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-crystal-store',
   templateUrl: './crystal-store.component.html',
-  styleUrls: ['./crystal-store.component.scss']
+  styleUrls: ['./crystal-store.component.scss'],
 })
 export class CrystalStoreComponent {
-
+  products = [
+    {
+      title: 'Kamdhenu Cow with Calf Idol - Gold & Silver Plated',
+      image: 'crystal_1.png',
+      rating: 4.5,
+      reviews: 2326,
+      price: 2499,
+    },
+    {
+      title: 'Kamdhenu Cow with Calf Idol - Gold & Silver Plated',
+      image: 'crystal_2.png',
+      rating: 4.7,
+      reviews: 1840,
+      price: 2999,
+    },
+    {
+      title: 'Kamdhenu Cow with Calf Idol - Gold & Silver Plated',
+      image: 'crystal_3.png',
+      rating: 4.6,
+      reviews: 2010,
+      price: 2799,
+    },
+    {
+      title: 'Kamdhenu Cow with Calf Idol - Gold & Silver Plated',
+      image: 'crystal_4.png',
+      rating: 4.8,
+      reviews: 3200,
+      price: 3499,
+    },
+  ];
 }
